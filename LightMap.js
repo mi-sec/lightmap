227ac34c3e209b84817b6906bd5d14077a117f16
Nick Soggin <nicksoggin@gmail.com>
2018-06-12 08:55:49 -0400

    added Symbol.search and Symbol.replace

--- LightMap.js
+++ LightMap.js
@@ -5,52 +5,54 @@
  *******************************************************************************************************/
 'use strict';
 
+const { version } = require( './package' );
+
 class LightMap extends Map
 {
 	filter( fn )
 	{
 		const arr = new LightMap();
-		
+
 		for( const [ key, value ] of this[ Symbol.iterator ]() ) {
 			if( fn( value, key, this ) ) {
 				arr.set( key, value );
 			}
 		}
-		
+
 		return arr;
 	}
-	
+
 	map( fn )
 	{
 		const arr = new LightMap();
-		
+
 		for( const [ key, value ] of this[ Symbol.iterator ]() ) {
 			let entry = fn( value, key, this );
-			
+
 			if( !entry ) {
 				entry = [ undefined, undefined ];
 			}
-			
+
 			arr.set( entry[ 0 ] || key, entry[ 1 ] || value );
 		}
-		
+
 		return arr;
 	}
-	
+
 	reduce( fn, r, iterator = this[ Symbol.iterator ]() )
 	{
 		for( const [ key, value ] of iterator ) {
 			r = fn( r, [ key, value ], key, this );
 		}
-		
+
 		return r;
 	}
-	
+
 	sortKeys( fn )
 	{
 		const keys = [ ...this.keys() ].sort( fn );
 		let i      = 0;
-		
+
 		return this.map(
 			( v, k, iterator ) => {
 				const key = keys[ i++ ];
@@ -58,8 +60,8 @@ class LightMap extends Map
 			}
 		);
 	}
-	
-	// TODO::: get back to this
+
+	// TODO::: [sortValues] get back to this
 	// sortValues( fn )
 	// {
 	// 	const
@@ -91,20 +93,81 @@ class LightMap extends Map
 	//
 	// 	return arr;
 	// }
-	
-	
-	toString()
+
+	static version()
 	{
-		return JSON.stringify(
-			this.reduce(
-				( r, [ k, v ] ) => {
-					r.push( [ k.toString(), v.toString() ] );
-					return r;
-				}, []
-			)
+		return `v${ version }`;
+	}
+
+	mapToArray()
+	{
+		return this.reduce(
+			( r, [ k, v ] ) => {
+				if( v instanceof LightMap ) {
+					v = v.toJSON();
+				}
+
+				r.push( [ k, v ] );
+				return r;
+			}, []
 		);
 	}
-	
+
+	toJSON()
+	{
+		return this.mapToArray();
+	}
+
+	toString()
+	{
+		return JSON.stringify( this.mapToArray() );
+	}
+
+	indexOf( n )
+	{
+		let i = -1;
+
+		return this.reduce(
+			( r, [ k ] ) => {
+				if( r === -1 ) {
+					i++;
+
+					if( k === n ) {
+						r = i;
+					}
+				}
+
+				return r;
+			}, -1
+		);
+	}
+
+	// TODO::: [equals] get back to this
+	// equals( n )
+	// {
+	// 	if( n instanceof LightMap ) {
+	// 		if( this.size !== n ) {
+	// 			return false;
+	// 		}
+	// 	}
+	// 	return false;
+	// }
+
+	[ Symbol.search ]( n )
+	{
+		return this.indexOf( n );
+	}
+
+	[ Symbol.replace ]( n )
+	{
+		return this.reduce(
+			( r, [ k, v ] ) => {
+				r = r.replace( k, v );
+				return r;
+			}, n
+		);
+	}
+
 	[ Symbol.toPrimitive ]( n )
 	{
 		if( n === 'string' ) {
@@ -117,17 +180,17 @@ class LightMap extends Map
 			return this.toString();
 		}
 	}
-	
+
 	get [ Symbol.toStringTag ]()
 	{
 		return this.constructor.name;
 	}
-	
+
 	static get [ Symbol.species ]()
 	{
-		return LightMap;
+		return Map;
 	}
-	
+
 	static [ Symbol.hasInstance ]( instance )
 	{
 		return instance.constructor.name === 'LightMap';
--- test/index.test.js
+++ test/index.test.js
@@ -11,26 +11,13 @@ const
 	LightMap = require( '../index' );
 
 describe( 'LightMap', () => {
-	
-	it( 'Symbol.toStringTag should equal LightMap',
-		() => expect( Object.prototype.toString.call( new LightMap() ) ).to.eq( '[object LightMap]' )
-	);
-	
-	it( 'Symbol.hasInstance should be false if instance of Map',
-		() => expect( new Map() instanceof LightMap ).to.eq( false )
-	);
-	
-	it( 'Symbol.hasInstance should be true if instance of LightMap',
-		() => expect( new LightMap() instanceof LightMap ).to.eq( true )
-	);
-	
 	it( 'LightMap.filter should pass keys and values',
 		() => {
 			const _ = new LightMap();
 			let k, v, self;
-			
+
 			_.set( 'key', 'value' );
-			
+
 			_.filter(
 				( _v, _k, _self ) => {
 					v    = _v;
@@ -38,23 +25,23 @@ describe( 'LightMap', () => {
 					self = _self;
 				}
 			);
-			
+
 			expect( k ).to.eq( 'key' );
 			expect( v ).to.eq( 'value' );
 			expect( self instanceof LightMap ).to.eq( true );
 		}
 	);
-	
+
 	it( 'LightMap.filter should return new LightMap of filtered items',
 		() => {
 			const _ = new LightMap();
 			_.set( 'key', 'value' );
 			_.set( 'key1', 'value1' );
-			
+
 			const result = _.filter(
 				( v, k ) => k === 'key'
 			);
-			
+
 			expect( result instanceof LightMap ).to.eq( true );
 			expect( result.has( 'key' ) ).to.eq( true );
 			expect( result.get( 'key' ) ).to.eq( 'value' );
@@ -64,14 +51,14 @@ describe( 'LightMap', () => {
 			expect( _.get( 'key1' ) ).to.eq( 'value1' );
 		}
 	);
-	
+
 	it( 'LightMap.map should pass keys and values',
 		() => {
 			const _ = new LightMap();
 			let k, v, self;
-			
+
 			_.set( 'key', 'value' );
-			
+
 			_.map(
 				( _v, _k, _self ) => {
 					v    = _v;
@@ -79,35 +66,35 @@ describe( 'LightMap', () => {
 					self = _self;
 				}
 			);
-			
+
 			expect( k ).to.eq( 'key' );
 			expect( v ).to.eq( 'value' );
 			expect( self instanceof LightMap ).to.eq( true );
 		}
 	);
-	
+
 	it( 'LightMap.map should return new LightMap of mapped items',
 		() => {
 			const _ = new LightMap();
 			_.set( 'key', 'value' );
-			
+
 			const result = _.map(
 				( v, k ) => [ k, v + 1 ]
 			);
-			
+
 			expect( result instanceof LightMap ).to.eq( true );
 			expect( _.get( 'key' ) ).to.eq( 'value' );
 			expect( result.get( 'key' ) ).to.eq( 'value1' );
 		}
 	);
-	
+
 	it( 'LightMap.reduce should pass return item, keys, and values',
 		() => {
 			const _ = new LightMap();
 			let r, k, v, key, self;
-			
+
 			_.set( 'key', 'value' );
-			
+
 			_.reduce(
 				( _r, [ _k, _v ], _key, _self ) => {
 					r    = _r;
@@ -117,7 +104,7 @@ describe( 'LightMap', () => {
 					self = _self;
 				}, 'test'
 			);
-			
+
 			expect( r ).to.eq( 'test' );
 			expect( k ).to.eq( 'key' );
 			expect( v ).to.eq( 'value' );
@@ -125,12 +112,12 @@ describe( 'LightMap', () => {
 			expect( self instanceof LightMap ).to.eq( true );
 		}
 	);
-	
+
 	it( 'LightMap.reduce should return string of keys and values',
 		() => {
 			const _ = new LightMap();
 			_.set( 'key', 'value' );
-			
+
 			const result = _.reduce(
 				( r, [ k, v ] ) => {
 					r += `Key: ${ k }\n`;
@@ -138,20 +125,20 @@ describe( 'LightMap', () => {
 					return r;
 				}, ''
 			);
-			
+
 			expect( result ).to.eq( 'Key: key\nValue: value\n' );
 		}
 	);
-	
+
 	it( 'LightMap.sortKeys should sort keys alphanumerically',
 		() => {
 			const _ = new LightMap();
 			_.set( 'key2', 'value2' );
 			_.set( 'key1', 'value1' );
 			_.set( 'key', 'value' );
-			
+
 			const result = _.sortKeys();
-			
+
 			expect( [ ..._.keys() ][ 0 ] ).to.eq( 'key2' );
 			expect( [ ..._.keys() ][ 1 ] ).to.eq( 'key1' );
 			expect( [ ..._.keys() ][ 2 ] ).to.eq( 'key' );
@@ -160,34 +147,89 @@ describe( 'LightMap', () => {
 			expect( [ ...result.keys() ][ 2 ] ).to.eq( 'key2' );
 		}
 	);
-	
+
+	it( 'LightMap.mapToArray and LightMap.toJSON should return a tuple array',
+		() => {
+			const _ = new LightMap();
+			_.set( 'key', new LightMap( [ [ 'key1', 'value1' ] ] ) );
+
+			const
+				result  = _.toJSON(),
+				result1 = _.mapToArray();
+
+			expect( result[ 0 ].length ).to.eq( 2 );
+			expect( result[ 0 ][ 0 ] ).to.eq( 'key' );
+			expect( result[ 0 ][ 1 ][ 0 ].length ).to.eq( 2 );
+			expect( result[ 0 ][ 1 ][ 0 ][ 0 ] ).to.eq( 'key1' );
+			expect( result[ 0 ][ 1 ][ 0 ][ 1 ] ).to.eq( 'value1' );
+			expect( result ).to.deep.eq( result1 );
+		}
+	);
+
+	it( 'Symbol.search and LightMap.indexOf should return the insert index of the specified key',
+		() => {
+			const _ = new LightMap();
+			_.set( 'key', 'value' );
+			_.set( 'key1', 'value1' );
+			_.set( 'key2', 'value2' );
+
+			expect( _.indexOf( 'key1' ) ).to.eq( 1 );
+			expect( _.indexOf( 'key3' ) ).to.eq( -1 );
+			expect( 'key1'.search( _ ) ).to.eq( 1 );
+			expect( 'key3'.search( _ ) ).to.eq( -1 );
+		}
+	);
+
+	it( 'Symbol.replace should replace a specified string with the value of the ',
+		() => {
+			const _ = new LightMap();
+			_.set( '{{ name }}', 'LightMap' );
+			_.set( '{{ version }}', LightMap.version() );
+
+			expect( 'Module: {{ name }} {{ version }}'.replace( _ ) )
+				.to.eq( `Module: LightMap ${ LightMap.version() }` );
+		}
+	);
+
 	it( 'Symbol.toPrimitive( string ) and LightMap.toString should return JSON.stringify expected results',
 		() => {
 			const _ = new LightMap();
 			_.set( 'key', new LightMap( [ [ 'a', 'b' ] ] ) );
-			
+
 			const
 				result  = _.toString(),
 				result1 = `${ _ }`;
-			
+
 			expect( typeof result === 'string' ).to.eq( true );
-			expect( result ).to.eq( '[["key","[[\\"a\\",\\"b\\"]]"]]' );
+			expect( result ).to.eq( '[["key",[["a","b"]]]]' );
 			expect( result === result1 ).to.eq( true );
 		}
 	);
-	
+
 	it( 'Symbol.toPrimitive( number ) and LightMap.size should return size of LightMap',
 		() => {
 			const _ = new LightMap();
 			_.set( 'key', 'value' );
-			
+
 			const
 				result  = _.size,
 				result1 = +_;
-			
+
 			expect( typeof result === 'number' ).to.eq( true );
 			expect( result ).to.eq( 1 );
 			expect( result === result1 ).to.eq( true );
 		}
+	);
+
+	it( 'Symbol.toStringTag should equal LightMap',
+		() => expect( Object.prototype.toString.call( new LightMap() ) ).to.eq( '[object LightMap]' )
+	);
+	
+	it( 'Symbol.hasInstance should be false if instance of Map',
+		() => expect( new Map() instanceof LightMap ).to.eq( false )
+	);
+
+	it( 'Symbol.hasInstance should be true if instance of LightMap',
+		() => expect( new LightMap() instanceof LightMap ).to.eq( true )
 	);
 } );
