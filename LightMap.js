aae26bc2b7c1b821fe96868cb6e6d9ce59de9ca3
iskore <nicksoggin@gmail.com>
2019-02-23 14:28:01 -0500

    fixing docs

--- LightMap.js
+++ LightMap.js
@@ -335,7 +335,6 @@ class LightMap extends Map
 	 */
 	toObject()
 	{
-		console.log( this );
 		return this.reduce(
 			( r, [ k, v ] ) => {
 				if( v instanceof LightMap ) {
@@ -362,7 +361,7 @@ class LightMap extends Map
 	 * indexOf
 	 * @description
 	 * returns the first index at which a given element can be found in the array, or -1 if it is not present
-	 * @param {string} n - key naem to search for
+	 * @param {string} n - key name to search for
 	 * @return {number} - index of the element
 	 */
 	indexOf( n )
